5ed08d4f507747f1c09ff303497d8e753616e85e
chithraksha16 <chithrakshakharvi@gmail.com>
2025-08-10 20:53:21 +0530

    updated day model

--- backend/src/models/day.model.ts
+++ backend/src/models/day.model.ts
@@ -1,11 +1,61 @@
-import mongoose, {Document, Schema } from "mongoose";
+import mongoose, {Document, Schema,Types } from "mongoose";
 
-interface IDay{
-    userId:Schema.Types.ObjectId,
-    entries: 
 
+type task={
+    userId:Types.ObjectId,
+    title:string,
+    description:string,
+    duration:number,
+    tag:string,
+    createdAt:Date
 }
 
-const daySchema=new Schema({
+interface IDay extends Document{
+    userId:Types.ObjectId,
+    entries: task[],
+}
 
+
+
+const daySchema=new Schema<IDay>({
+    userId:{
+        type:Schema.Types.ObjectId,
+        required:true
+    },
+    entries:{
+        type:[{
+            userId:{
+                type:Schema.Types.ObjectId,
+                required:true
+            },
+            title:{
+                type:String,
+                required:true
+            },
+            description:{
+                type:String,
+                required:true
+            },
+            duration:{
+                type:Number,
+                required:true
+            },
+            tag:{
+                type:String,
+                required:true
+            },
+            createdAt:{
+                type:Date,
+                default: new Date
+            }
+        }],
+        required:true
+    }
+
+},{
+    timestamps:true
 })
+
+
+const Day=mongoose.model<IDay>("Day",daySchema)
+export default Day
