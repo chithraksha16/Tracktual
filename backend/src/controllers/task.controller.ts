f9932f2c2523a45b0a9d928e779972a61f8b73fc
chithraksha16 <chithrakshakharvi@gmail.com>
2025-08-12 20:52:18 +0530

    added postTask

---
+++ backend/src/controllers/task.controller.ts
@@ -0,0 +1,32 @@
+import Day from "../models/day.model";
+import Work from "../models/work.model";
+import { Request,Response } from "express";
+
+type TaskBody={
+    title:string,
+    description:string,
+    tag:string,
+    duration:number
+}
+
+ export const postTask=async(req:Request<{},{},TaskBody>,res:Response):Promise<void>=>{
+    const {title,description,tag,duration}=req.body;
+    try{
+        if(!title || !description || !tag || !duration){
+            return;
+        }
+        const task=await Work.create({
+            title,
+            description,
+            tag,
+            duration
+        })
+        res.status(201).json({message:"Task Posted",work:task})
+    }
+    catch(error:unknown){
+        console.error("Task not posted",error)
+        res.status(400).json({message:"Task not Posted"})
+    }
+
+
+}
