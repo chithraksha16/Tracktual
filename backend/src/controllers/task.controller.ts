065a7b846ef8eeafd948673b4975385bed8e14a6
chithraksha16 <chithrakshakharvi@gmail.com>
2025-08-19 21:12:57 +0530

    added dayTime and endTime to the taskController

--- backend/src/controllers/task.controller.ts
+++ backend/src/controllers/task.controller.ts
@@ -11,17 +11,36 @@ type TaskBody={
 
  export const postTask=async(req:Request<{},{},TaskBody>,res:Response):Promise<void>=>{
     const {title,description,tag,duration}=req.body;
+    const userId=req.user
     try{
         if(!title || !description || !tag || !duration){
             res.status(400).json({message:"All feilds are required"})
             return;
         }
         const task=await Work.create({
+            userId:userId,
             title,
             description,
             tag,
             duration
         })
+
+        const now=new Date(new Date())
+        const startDay=new Date(now.setHours(0,0,0,0))
+        const endDay=new Date(now.setHours(23,59,59,999))
+
+
+        await Day.findOneAndUpdate({
+            userId,
+            startDay,
+            endDay
+        },{
+            $setOnInsert: { startDay, endDay },
+        $addToSet: { entries: task._id }
+        },{
+            upsert: true, new: true 
+        })
+
         res.status(201).json({message:"Task Posted",work:task})
     }
     catch(error:unknown){
