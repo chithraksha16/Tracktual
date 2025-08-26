1058e75fc6a660c558c068a8a9152d0355571df6
chithraksha16 <chithrakshakharvi@gmail.com>
2025-08-26 19:02:02 +0530

    corret file name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
-
+import { Route,Routes } from 'react-router-dom'
 import './App.css'
-
+import Signup from './Pages/Signup'
 function App() {
 
   return (
     <>
-      
+      <div>
+        <Routes>
+          <Route path='/signup' element={<Signup/>}/>
+        </Routes>
+
+      </div>
     </>
   )
 }
---
+++ frontend/src/Pages/Signup.tsx
@@ -0,0 +1,11 @@
+const Signup = () => {
+  return (
+    <div className='max-w-lg w-full border px-5'>
+      <div>
+      hello
+      </div>
+    </div>
+  )
+}
+
+export default Signup
