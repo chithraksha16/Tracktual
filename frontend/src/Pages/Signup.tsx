e36360fb875bb05ff0f70689cb149697ea3308fd
chithraksha16 <chithrakshakharvi@gmail.com>
2025-08-27 22:27:42 +0530

    updated signup

--- frontend/src/Pages/Signup.tsx
+++ frontend/src/Pages/Signup.tsx
@@ -1,25 +1,25 @@
 const Signup = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
-    <div className='max-w-md w-full border px-5'>
-      <div>
-      <div className="text-center">
-        <h1>Create a account</h1>
-        <p></p>
+    <div className='max-w-sm w-full border p-5 rounded-lg'>
+      <div className="space-y-2">
+      <div className="text-center ">
+        <h1 className="text-lg font-medium">Create a account</h1>
+        <p className="text-xs">Sign up to start your journey with Tracktual.</p>
       </div>
-      <div>
+      <div className="flex flex-col px-4 gap-2">
         <label htmlFor="name1">Name:</label>
         <input type="text" name="name" id="name1"  />
       </div>
-      <div>
+      <div className="flex flex-col px-4  gap-2">
         <label htmlFor="email1">Email:</label>
         <input type="email" name="email" id="email1"  />
       </div>
-      <div>
+      <div className="flex flex-col px-4  gap-2">
         <label htmlFor="password1">Password:</label>
         <input type="password" name="password" id="password1" />
       </div>
-      <div>
+      <div className="flex justify-center">
         <button>Signup</button>
       </div>
       </div>
