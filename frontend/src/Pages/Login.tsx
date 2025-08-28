425ba2d4001bc4768727b01c9c5ff195d3c1a025
chithraksha16 <chithrakshakharvi@gmail.com>
2025-08-28 21:54:32 +0530

    added login

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -1,8 +1,26 @@
 
 const Login = () => {
   return (
-    <div>
-      login
+    <div className="flex justify-center items-center min-h-screen">
+    <div className='max-w-sm w-full border p-5 rounded-lg'>
+      <div className="space-y-2">
+      <div className="text-center ">
+        <h1 className="text-lg font-medium">Create a account</h1>
+        <p className="text-xs">Sign up to start your journey with Tracktual.</p>
+      </div>
+      <div className="flex flex-col px-4  gap-2">
+        <label htmlFor="email1">Email:</label>
+        <input type="email" name="email" id="email1"  />
+      </div>
+      <div className="flex flex-col px-4  gap-2">
+        <label htmlFor="password1">Password:</label>
+        <input type="password" name="password" id="password1" />
+      </div>
+      <div className="flex justify-center">
+        <button>Signup</button>
+      </div>
+      </div>
+    </div>
     </div>
   )
 }
