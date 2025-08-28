021cefc49439e15c1de01562d67c4cc558419e62
chithraksha16 <chithrakshakharvi@gmail.com>
2025-08-28 21:58:28 +0530

    setted home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { Route,Routes } from 'react-router-dom'
 import './App.css'
 import Signup from './Pages/Signup'
 import Login from './Pages/Login'
+import Home from './Pages/Home'
 function App() {
 
   return (
     <>
       <div>
         <Routes>
+          <Route path='/' element={<Home/>}/>
           <Route path='/signup' element={<Signup/>}/>
           <Route path='/login'  element={<Login/>}/>
         </Routes>
