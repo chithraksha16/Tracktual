fd7d06676d128cf616c7d2b969b6d0f05f3110e2
chithraksha16 <chithrakshakharvi@gmail.com>
2025-09-02 21:24:23 +0530

    updated login

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -2,22 +2,22 @@
 const Login = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
-    <div className='max-w-sm w-full border p-5 rounded-lg'>
+    <div className='max-w-xs w-full border p-5 rounded-lg'>
       <div className="space-y-2">
-      <div className="text-center ">
-        <h1 className="text-lg font-medium">Create a account</h1>
-        <p className="text-xs">Sign up to start your journey with Tracktual.</p>
+      <div className="text-center p-3 ">
+        <h1 className="text-lg font-medium">Welcome Back</h1>
+        <p className="text-xs">login to continue your journey with Tracktual.</p>
       </div>
       <div className="flex flex-col px-4  gap-2">
         <label htmlFor="email1">Email:</label>
-        <input type="email" name="email" id="email1"  />
+        <input className="w-full border border-gray-500 rounded"  type="email" name="email" id="email1"  />
       </div>
       <div className="flex flex-col px-4  gap-2">
         <label htmlFor="password1">Password:</label>
-        <input type="password" name="password" id="password1" />
+        <input className="w-full border border-gray-500 rounded" type="password" name="password" id="password1" />
       </div>
-      <div className="flex justify-center">
-        <button>Signup</button>
+      <div className="flex justify-center py-1">
+        <button className="px-15 py-0.5 bg-blue-600 rounded">Login</button>
       </div>
       </div>
     </div>
