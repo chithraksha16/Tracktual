b2f32cc88adadc0b76588cefa1cb3d6db222df6a
chithraksha16 <chithrakshakharvi@gmail.com>
2025-09-18 17:06:55 +0530

    added zod validator

---
+++ backend/src/middleware/validate.middleware.ts
@@ -0,0 +1,19 @@
+import { error } from "console";
+import { Request,Response,NextFunction } from "express";
+import { ZodSchema } from "zod/v3";
+
+
+export const validate=(schema:ZodSchema<any>)=>(req:Request,res:Response,next:NextFunction)=>{
+
+    const result=schema.safeParse({
+        body:req.body,
+        query:req.query,
+        params:req.params
+    });
+    if(!result.success){
+        res.status(400).json({errors:result.error.format()});
+    }
+
+    return next()
+
+}
