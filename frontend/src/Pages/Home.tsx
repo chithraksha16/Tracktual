d45f4f820272c1d618d4db75cf4a7899f969f7d3
chithraksha16 <chithrakshakharvi@gmail.com>
2025-10-20 21:44:58 +0530

    updated v10

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -4,8 +4,9 @@ const Home = () => {
   return (
     <div>
     <div className=" w-full flex justify-between p-2">
-        <div className="w-full p-4 sm:p-6">
+        <div className="w-full p-4 sm:p-6 space-y-2">
           <h1 className="sm:text-2xl text-xl font-bold text-center">Track Your Daily Work</h1>
+          <p className="sm:text-base text-sm font-mono text-center px-2">Keep track to stay focused and productive.</p>
         </div>
       
     </div>
