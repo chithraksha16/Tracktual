f34bfcb7a28124983eb5a0368704e5302f5d03b6
chithraksha16 <chithrakshakharvi@gmail.com>
2025-10-29 22:15:39 +0530

    updated v14

---
+++ frontend/src/features/auth/authSlice.ts
@@ -0,0 +1,54 @@
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+
+const API_URL="http://localhost:5001/api"
+
+ interface User{
+        name:string;
+        email:string;
+        password:string;
+ }
+interface authState{
+    user:User | null,
+    token:string | null;
+    loading:boolean,
+    error:string | null,
+}
+const initialState:authState={
+        user:null,
+        token:localStorage.getItem("token") || null,
+        loading:false,
+        error:null
+
+}
+
+export const signupUser = createAsyncThunk(
+  "auth/signupUser",
+  async (
+    newUser: { name: string; email: string; password: string },
+    { rejectWithValue }
+  ) => {
+    try {
+      const res = await axios.post(API_URL, newUser);
+
+      // Example response: { token: "...", user: { name, email } }
+      localStorage.setItem("token", res.data.token);
+
+      return res.data;
+    } catch (error: any) {
+      return rejectWithValue(
+        error.response?.data?.message || "Signup failed. Please try again."
+      );
+    }
+  }
+);
+
+  const auth=createSlice({
+    name:"auth",
+    initialState,
+    reducers:{
+        
+
+    }
+  })
