e79ad44ff03c934002eb52c93ce49ed49261b928
chithraksha16 <chithrakshakharvi@gmail.com>
2026-02-09 21:20:09 +0530

    updated trackcard

--- frontend/src/Components/TrackCard.tsx
+++ frontend/src/Components/TrackCard.tsx
@@ -7,10 +7,10 @@ const TrackCard = () => {
   <div className="relative
               bg-white/10 backdrop-blur-xl
               border border-white/20
-              rounded-2xl
+              rounded-se-[40px] rounded-lg
               shadow-2xl
-              p-8 max-w-md text-white">
-    <h1 className="text-2xl font-semibold">Frontend state Mangement</h1>
+              p-6 w-md sm:w-sm max-w-md text-white h-36">
+    <h1 className="sm:text-xl text-lg font-semibold">Frontend state Mangement</h1>
     <p className="text-white/70 mt-2">
       working on te redux slice and hooks
     </p>
