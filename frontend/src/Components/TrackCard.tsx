a52eb071b87d90b868b2b25f744a5af18d01f4f3
chithraksha16 <chithrakshakharvi@gmail.com>
2026-02-24 11:58:27 +0530

    updated to new design and added new tools

--- frontend/src/Components/TrackCard.tsx
+++ frontend/src/Components/TrackCard.tsx
@@ -1,14 +1,15 @@
 import { IoMdTime } from "react-icons/io";
 import { LuTags } from "react-icons/lu";
 import { useState } from "react"
-
+import { PiStarFourFill } from "react-icons/pi";
+import { HiArrowLongRight } from "react-icons/hi2";
 
 type TrackCardProps = {
   text: string
   wordLimit?: number
 }
 
-const TrackCard = ({ text, wordLimit = 5 }: TrackCardProps) => {
+const TrackCard = ({ text, wordLimit = 6 }: TrackCardProps) => {
   const [isExpanded, setIsExpanded] = useState(false)
 
   const displayText = isExpanded
@@ -19,18 +20,21 @@ const TrackCard = ({ text, wordLimit = 5 }: TrackCardProps) => {
     <div className="flex">
       <div
         className={`relative
-          backdrop-blur-xl bg-radial-[at_50%_-10%] from-[#2E1339] from-19% to-black to-65%
+          backdrop-blur-xl bg-radial-[at_50%_-10%] from-[#1b0b23] from-19% to-black to-65%
           border-1 border-[#2E1339]
-          rounded-lg
+          rounded-2xl
           shadow-2xl
           p-6 w-md sm:w-sm max-w-md text-white ${isExpanded ? `h-auto`:`h-44`}`}
       >
-        <h1 className="sm:text-xl text-lg font-semibold ">
+        <div className="flex justify-end space-y-3">
+        <div className="text-white flex "><PiStarFourFill className="drop-shadow-[0_2px_2px_rgba(255,255,255,0.5)]" size={16}/> <PiStarFourFill className="mt-3 drop-shadow-[0_2px_2px_rgba(255,255,255,0.8)]" size={9}/></div>
+        </div>
+        <h1 className="sm:text-lg text-md font-Inter font-semibold tracking-wide ">
         <span>Frontend State Management</span>
         </h1>
 
       <div className={isExpanded ? ``:'flex items-center'}>
-        <p className="text-white/70 mt-2  ">
+        <p className="text-white/70 font-medium text-sm font-Inter mt-2  ">
           {displayText}
         
 
@@ -39,19 +43,19 @@ const TrackCard = ({ text, wordLimit = 5 }: TrackCardProps) => {
             onClick={() => setIsExpanded(!isExpanded)}
             className=" text-sm text-blue-600 hover:underline font-medium"
           >
-            {isExpanded ? "read less" : "read more"}
+            {isExpanded ? " read less" : " read more"}
           </button>
         )}
         </p>
         </div>
 
-        <div className="flex mt-2 px-2 gap-5">
-          <h3 className="text-lg flex items-center gap-1.5"><IoMdTime size={20}/> <span>1hr 30min</span></h3>
-          <h3 className="text-lg flex items-center gap-1.5"><LuTags size={20}/> <span>#coding</span></h3>
+        <div className="flex mt-3  gap-10">
+          <h3 className="text-md flex font-Inter items-center gap-1.5"><IoMdTime size={20}/> <span>1hr 30min</span></h3>
+          <h3 className="text-md flex font-Inter  items-center gap-1.5"><LuTags size={20}/> <span>#coding</span></h3>
         </div>
-        {/* <div className="mt-1">
-          <button className="px-4 py-1 bg-red-800 text-white rounded">Delete</button>
-        </div> */}
+        <div className=" flex justify-end">
+          <button className=" font-extralight flex  items-center gap-2 drop-shadow-[0_3px_4px_rgba(255,255,255,0.2)]"><HiArrowLongRight/> <span className="text-xs">Delete</span></button>
+        </div>
       </div>
     </div>
   )
