cd16dc8c33d6bebbcf3262bfe9a822df6f40c666
chithraksha16 <chithrakshakharvi@gmail.com>
2026-04-06 18:47:11 +0530

    added toLocaleDateString

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -44,7 +44,11 @@ const Home = () => {
               </div>
               <div className='sm:px-5   mt-5 flex  justify-center sm:justify-normal  gap-5 sm:gap-10 flex-wrap'>
               {block?.map((blocks,idx)=>(
-                <button key={idx} className='cursor-auto' type='submit' onClick={()=>handleParticularTask(blocks.endDay)}>
+                <button key={idx} className='cursor-auto' type='submit' onClick={()=>handleParticularTask(new Date(blocks.endDay).toLocaleDateString("en-IN",{
+                  day: "numeric",
+                  month: "short",
+                  year: "numeric"
+                }))}>
                   <div  className='sm:w-40 sm:h-24 md:w-32 md:h-20 w-32 h-20  border rounded-lg bg-linear-to-br from-25% from-[#000] to-80% to-[#3B022B] flex justify-center items-center space-y-2 '>
                   
                   <h1 className='text-white sm:text-xl text-lg '>{new Date(blocks.startDay).toLocaleDateString("en-IN", {
