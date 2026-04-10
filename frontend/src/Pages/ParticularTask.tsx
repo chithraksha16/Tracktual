6acc3cff0d11cf19a4160b46053252ff7e99c1aa
chithraksha16 <chithrakshakharvi@gmail.com>
2026-04-10 19:02:52 +0530

    added styles

--- frontend/src/Pages/ParticularTask.tsx
+++ frontend/src/Pages/ParticularTask.tsx
@@ -9,7 +9,7 @@ const ParticularTask = () => {
   return (
     <div className="w-full bg-black flex justify-center">
       <div className=' mt-10 w-full max-w-3xl h-18 border flex justify-center items-center bg-linear-to-r from-black to-[#3B022B] rounded-lg'>
-        <h1 className="text-2xl">{pDate.map((pdate)=>pdate.endDay)}</h1>
+        <h1 className="sm:text-2xl text-xl">{pDate.map((pdate)=>pdate.endDay)}</h1>
       </div>
     </div>
   )
