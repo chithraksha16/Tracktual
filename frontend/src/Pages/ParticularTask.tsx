4e729b5a88a238fd353fc0902cc869681683ba9d
chithraksha16 <chithrakshakharvi@gmail.com>
2026-04-28 18:34:19 +0530

    updated responsiveness

--- frontend/src/Pages/ParticularTask.tsx
+++ frontend/src/Pages/ParticularTask.tsx
@@ -52,12 +52,12 @@ const ParticularTask = () => {
 
       <div className="flex flex-wrap sm:justify-start justify-center  sm:px-20 px-2 gap-5 mt-15">
         {pDate.map((pdates)=>pdates.entries.map((entries,idx)=>(
-          <div key={idx} className="sm:w-sm  w-full h-48 px-10 border rounded-3xl space-y-3 ">
+          <div key={idx} className="sm:w-sm  w-full sm:h-48 h-52 sm:px-10 px-4 border rounded-3xl space-y-3  ">
             <h1 className="pt-4 text-lg sm:text-2xl font-mono ">{capitalizeFirst(entries.title)}</h1>
             <p className="w-full sm:text-md text-sm ">{capitalizeFirst(entries.description)}</p>
-            <div className="flex gap-10 ml-[-2]">
-              <h3 className="text-xl flex justify-center items-center gap-2"><MdAvTimer size={23} /><span className="flex">{entries.hours}hr {"  "} {entries.minutes} min</span></h3>
-              <h3 className="text-xl flex justify-center items-center gap-2"><IoPricetagsOutline /><span>{checkHashtag(entries.tag)}</span></h3>
+            <div className="flex gap-10 ml-[-2] py-2 mt-4">
+              <h3 className="sm:text-xl flex justify-center items-center gap-2"><MdAvTimer size={23} /><span className="flex">{entries.hours}hr {"  "} {entries.minutes} min</span></h3>
+              <h3 className="sm:text-xl flex justify-center items-center gap-2"><IoPricetagsOutline /><span>{checkHashtag(entries.tag)}</span></h3>
             </div>
             <div className="flex justify-end items-end  mt-8">
               <div className="flex justify-center items-center gap-2">
